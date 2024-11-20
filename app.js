46941252f19d88e84f0fc4c8e2aa11af211b390f
Emmanuel Okeowo <okeowoemmanuelm@gmail.com>
2024-11-20 16:48:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 document.getElementById('partyForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
+    
     // Prevent form from submitting normally
-  
     const name = document.getElementById('name').value.trim();
     const age = parseInt(document.getElementById('age').value, 10);
     const gender = document.getElementById('gender').value;
   
     const messageDiv = document.getElementById('message');
     messageDiv.textContent = ''; 
+    
     // Clear previous messages
     messageDiv.classList.remove('success', 'error'); 
+    
     // Reset message classes
-  
-    if (age < 18) {
+      if (age < 18) {
       messageDiv.classList.add('error');
       messageDiv.textContent = 'You must be at least 18 years old to register.';
     } else if (gender !== 'Male') {
